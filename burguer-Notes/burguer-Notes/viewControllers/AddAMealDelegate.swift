54424f57f8668156086dc8f41e42934bd108a1de
Danilo Requena <danilo.requena26@gmail.com>
2018-12-18 23:43:47 -0200

    Criando e redefinindo o delegate (AddAMealDelegate)

---
+++ burguer-Notes/burguer-Notes/viewControllers/AddAMealDelegate.swift
@@ -0,0 +1,15 @@
+//
+//  ViewControllerDelegate.swift
+//  burguer-Notes
+//
+//  Created by Danilo Requena on 17/12/18.
+//  Copyright © 2018 Danilo Requena. All rights reserved.
+//
+
+import Foundation
+
+protocol delegate {
+    func add (_ meals: Meal)
+    
+    
+}
