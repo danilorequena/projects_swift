885eb2f648901f3d0521cc912b680a5ad2f073c0
Danilo Requena <danilo.requena26@gmail.com>
2019-01-23 10:11:26 -0200

    Implementado a TableView e populado ela com valores fixos

--- TravelNow/TravelNow/ViewController.swift
+++ TravelNow/TravelNow/ViewController.swift
@@ -8,11 +8,28 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource {
+
+    @IBOutlet weak var travelTable: UITableView!
+    
+    let travelList: Array<String> = ["Rio de Janeiro", "Ceará", "Paraná", "Rio Grande do Sul", "Porto Alegre", "USA"]
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        self.travelTable.dataSource = self
         // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return travelList.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        cell.textLabel?.text = travelList[indexPath.row]
+        
+        return cell
     }
 
 
