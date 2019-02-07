3fe28db38ee40da9e8dff01c52c83c74e37d4ae5
Danilo Requena <danilo.requena@gmail.com>
2019-02-07 11:24:19 -0200

    Implementado reload na tableView e com as fotos redondas

--- agenda/Agenda/Home/Controller/HomeTableViewController.swift
+++ agenda/Agenda/Home/Controller/HomeTableViewController.swift
@@ -7,18 +7,27 @@
 //
 
 import UIKit
+import CoreData
 
-class HomeTableViewController: UITableViewController, UISearchBarDelegate {
+class HomeTableViewController: UITableViewController, UISearchBarDelegate, NSFetchedResultsControllerDelegate {
     
     //MARK: - Variáveis
     
+    var context: NSManagedObjectContext {
+        let appDelegate = UIApplication.shared.delegate as! AppDelegate
+        
+        return appDelegate.persistentContainer.viewContext
+    }
     let searchController = UISearchController(searchResultsController: nil)
+    var managerResults:NSFetchedResultsController<Aluno>?
     
     // MARK: - View Lifecycle
 
     override func viewDidLoad() {
         super.viewDidLoad()
         self.configuraSearch()
+        self.catchStudent()
+        
     }
     
     // MARK: - Métodos
@@ -28,17 +37,42 @@ class HomeTableViewController: UITableViewController, UISearchBarDelegate {
         self.searchController.dimsBackgroundDuringPresentation = false
         self.navigationItem.searchController = searchController
     }
+    
+    func catchStudent() {
+        let searchStudent:NSFetchRequest<Aluno> = Aluno.fetchRequest()
+        let orderByName = NSSortDescriptor(key: "nome", ascending: true)
+        searchStudent.sortDescriptors = [orderByName]
+        
+        managerResults = NSFetchedResultsController(fetchRequest: searchStudent, managedObjectContext: context, sectionNameKeyPath: nil, cacheName: nil)
+        managerResults?.delegate = self
+        
+        do{
+            try managerResults?.performFetch()
+        } catch {
+            print(error.localizedDescription)
+        }
+        
+    }
 
     // MARK: - Table view data source
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 0
+        guard let studantListCount = managerResults?.fetchedObjects?.count else { return 0 }
+    
+        return studantListCount
     }
 
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "celula-aluno", for: indexPath) as! HomeTableViewCell
+        guard let aluno = managerResults?.fetchedObjects![indexPath.row] else { return cell }
+        cell.configStudant(aluno)
+        
 
         return cell
+    }
+    
+    override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 85
     }
 
     override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
@@ -48,6 +82,18 @@ class HomeTableViewController: UITableViewController, UISearchBarDelegate {
         } else if editingStyle == .insert {
             // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
         }    
+    }
+    
+    // MARK: - FetchedResultsControllerDelegate
+    
+    func controller(_ controller: NSFetchedResultsController<NSFetchRequestResult>, didChange anObject: Any, at indexPath: IndexPath?, for type: NSFetchedResultsChangeType, newIndexPath: IndexPath?) {
+        switch type {
+        case .delete:
+            //Implementar
+            break
+        default:
+            tableView.reloadData()
+        }
     }
 
 }
--- agenda/Agenda/Home/View/HomeTableViewCell.swift
+++ agenda/Agenda/Home/View/HomeTableViewCell.swift
@@ -15,6 +15,16 @@ class HomeTableViewCell: UITableViewCell {
     @IBOutlet weak var imageAluno: UIImageView!
     @IBOutlet weak var labelNomeDoAluno: UILabel!
 
+    func configStudant(_ aluno: Aluno) {
+        labelNomeDoAluno.text = aluno.nome
+        imageAluno.layer.cornerRadius = imageAluno.frame.width / 2
+        imageAluno.layer.masksToBounds = true
+        
+        if let imageOfStudant = aluno.foto as? UIImage {
+            imageAluno.image = imageOfStudant
+        }
+    }
+    
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
